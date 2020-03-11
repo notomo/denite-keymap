c608f42204289cd147a2da033a14697d0e1ddde8
notomo <notomo.motono@gmail.com>
2020-03-11 09:05:30 +0900

    Format by black

--- rplugin/python3/denite/kind/keymap.py
+++ rplugin/python3/denite/kind/keymap.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 
@@ -6,75 +5,65 @@ from .file import Kind as File
 
 
 class Kind(File):
-
     def __init__(self, vim):
         super().__init__(vim)
 
-        self.name = 'keymap'
-        self.default_action = 'execute'
-        self.redraw_actions += ['delete', 'unmap']
-        self.persist_actions += ['delete', 'unmap']
+        self.name = "keymap"
+        self.default_action = "execute"
+        self.redraw_actions += ["delete", "unmap"]
+        self.persist_actions += ["delete", "unmap"]
 
     def action_execute(self, context):
-        target = context['targets'][0]
+        target = context["targets"][0]
 
         # execute only normal mode keymap
-        if 'n' not in target['action__mode']:
+        if "n" not in target["action__mode"]:
             return
 
         # ex. <CR> -> \<CR>
-        lhs = re.sub(
-            '<(?=(.+)>)',
-            '\\<',
-            target['action__lhs']
-        )
+        lhs = re.sub("<(?=(.+)>)", "\\<", target["action__lhs"])
 
         # ^\<Space> -> ^1\<Space>
         # help :normal
-        lhs = re.sub('(?=^\\\\<Space>)', '1', lhs)
+        lhs = re.sub("(?=^\\\\<Space>)", "1", lhs)
 
-        command = 'normal {}'.format(lhs)
+        command = "normal {}".format(lhs)
         self.vim.command('execute "{}"'.format(command))
 
     def action_delete(self, context):
-        for target in context['targets']:
+        for target in context["targets"]:
             # ex. mode = nox -> nunmap, ounmap, xunmap
-            for mode in target['action__mode']:
-                self.vim.command(
-                    '{}unmap {}'.format(
-                        mode,
-                        target['action__lhs']
-                    )
-                )
+            for mode in target["action__mode"]:
+                self.vim.command("{}unmap {}".format(mode, target["action__lhs"]))
 
     def action_open(self, context):
-        self._open(context, 'edit')
+        self._open(context, "edit")
 
     def action_drop(self, context):
-        self._open(context, 'drop')
+        self._open(context, "drop")
 
     def action_tabopen(self, context):
-        self._open(context, 'tabedit')
+        self._open(context, "tabedit")
 
     def _open(self, context, cmd):
         new_targets = []
         # FIXME <Space>, <lt>
-        escaped = '[]=|\\(){}<>%/&$^~@?'
-        for target in context['targets']:
-            lhs = target['action__lhs']
+        escaped = "[]=|\\(){}<>%/&$^~@?"
+        for target in context["targets"]:
+            lhs = target["action__lhs"]
             path = self._get_file_path(lhs)
             if path is None:
                 continue
 
-            pattern = '\\v\\c.*map.*{}\\s+{}'.format(
-                self.vim.call('escape', lhs, escaped),
-                self.vim.call('escape', target['action__rhs'], escaped)
+            pattern = "\\v\\c.*map.*{}\\s+{}".format(
+                self.vim.call("escape", lhs, escaped),
+                self.vim.call("escape", target["action__rhs"], escaped),
             )
-            target['action__path'] = path
-            target['action__pattern'] = pattern
+            target["action__path"] = path
+            target["action__pattern"] = pattern
             new_targets.append(target)
 
-        context['targets'] = new_targets
+        context["targets"] = new_targets
         super()._open(context, cmd)
 
     def action_unmap(self, context):
@@ -82,11 +71,13 @@ class Kind(File):
         self.action_delete(context)
 
     def _get_file_path(self, lhs):
-        cmd = 'verbose map {}'.format(lhs)
-        map_output = list(filter(
-            lambda x: x != '',
-            self.vim.call('denite_keymap#util#redir', cmd).split('\n')
-        ))
+        cmd = "verbose map {}".format(lhs)
+        map_output = list(
+            filter(
+                lambda x: x != "",
+                self.vim.call("denite_keymap#util#redir", cmd).split("\n"),
+            )
+        )
         if len(map_output) < 2:
             return None
 
